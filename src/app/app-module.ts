2964b3123db89d2dcfde1738e2d461ea6dbba232
bingo7894 <pokpong.padjunreed@gmail.com>
2026-03-21 11:16:49 +0700

    feat: root page

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Landing } from './landing/landing';
+import { SharedModule } from './shared/shared-module';
+import { Signup } from './signup/signup';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners()
-  ],
-  bootstrap: [App]
+  declarations: [App, Landing, Signup],
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Landing } from './landing/landing';
+import { Signup } from './signup/signup';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: Landing },
+  { path: 'signup', component: Signup },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
