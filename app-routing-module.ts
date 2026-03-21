2d3ebb109b9313d6aeb4fad1090b13f6e0246319
bingo7894 <pokpong.padjunreed@gmail.com>
2026-03-21 11:14:06 +0700

    first commit

---
+++ app-routing-module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Landing } from './landing/landing';
+import { Signup } from './signup/signup';
+
+const routes: Routes = [
+  { path: '', component: Landing },
+  { path: 'signup', component: Signup },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
